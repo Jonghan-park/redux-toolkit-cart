8fa2a27b807e2aca9c54240322739a746dcde302
Jonghan Park <jonghan.park@edu.sait.ca>
2022-10-19 10:26:05 -0600

    Update async function

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { clearCart } from "../features/cart/cartSlice";
 import { openModal } from "../features/modal/modalSlice";
 import CartItem from "./CartItem";
 
