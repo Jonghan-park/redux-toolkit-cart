b531f0daff9e9b807c62e38c07cf24d798f0af5b
Jonghan Park <jonghan.park@edu.sait.ca>
2022-10-18 21:26:56 -0600

    Update actions

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -11,8 +11,16 @@ const initialState = {
 const cartSlice = createSlice({
   name: "cart",
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+      // return { cartItems: [] };
+    },
+  },
 });
 
 // console.log(cartSlice);
+
+export const { clearCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
